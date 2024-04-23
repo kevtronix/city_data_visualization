97ed6562d8e04b962ca0560a146d70f8e9e598cc
Kevin Abraham <myservices@kabes.org>
2024-04-23 17:55:20 -0400

    incorporated more data sets for visualization and fixed issue with inability to display results

--- app/data.ts
+++ app/data.ts
@@ -3,6 +3,8 @@ import { matchSorter } from "match-sorter";
 export interface DataOptionType {
   id: string;
   label: string;
+  description: string;
+  referenceUrl: string;
   initialViewState: {
     longitude: number;
     latitude: number;
@@ -16,13 +18,17 @@ export interface DataOptionType {
 const DATASET: DataOptionType[] = [
   {
     id: "nyc-tree",
-    label: "NYC Tree Data",
+    label: "2015 NYC Tree Data",
+    description:
+      "Street tree data from the TreesCount! 2015 Street Tree Census, conducted by volunteers and staff organized by NYC Parks & Recreation and partner",
+    referenceUrl:
+      "https://data.cityofnewyork.us/Environment/2015-Street-Tree-Census-Tree-Data/pi5s-9p35",
     initialViewState: {
       longitude: -73.9710129,
-      latitude: 40.7637567,
+      latitude: 40.7637567, 
       zoom: 11,
-      pitch: 40.5,
-      bearing: -27,
+      pitch: 0,
+      bearing: 0,
     },
     dataUrl:
       "https://data.cityofnewyork.us/resource/5rq2-4hqu.json?$limit=10000&boroname=Manhattan",
@@ -30,31 +36,49 @@ const DATASET: DataOptionType[] = [
   {
     id: "la-active-business",
     label: "LA Active Business",
+    description:
+      "Listing of all active businesses currently registered with the Office of Finance",
+    referenceUrl:
+      "https://data.lacity.org/Administration-Finance/Listing-of-Active-Businesses/6rrh-rzua/about_data",
     initialViewState: {
       longitude: -118.4385022,
       latitude: 34.0825427,
-      zoom: 11,
+      zoom: 10,
     },
     dataUrl:
       "https://data.lacity.org/resource/6rrh-rzua.json?$limit=150000&$WHERE=within_box(location_1, 33.7035, -118.6682, 34.8233, -117.6464) AND location_1 IS NOT NULL",
   },
-  {
-    id: "westchester-facilities",
-    label: "W",
+   {
+    id: "nyc-wifi-hotspot",
+    label: "NYC Wifi Hotspots",
+    description:
+      "Listing of all hotspot locations in NYC",
+    referenceUrl:
+      "https://data.cityofnewyork.us/City-Government/NYC-Wi-Fi-Hotspot-Locations/yjub-udmw/about_data",
     initialViewState: {
-      longitude: -73.88315357141246,
-      latitude: 40.942970830130356,
+      longitude:  -73.9710129,
+      latitude: 40.7637567,
       zoom: 10,
-      pitch: 40.5,
-      bearing: -27,
     },
-    dataUrl: "https://health.data.ny.gov/resource/ba3n-bkk4.json",
+    dataUrl:
+      "https://data.cityofnewyork.us/resource/yjub-udmw.json",
+  },
+  {
+    id: "nyc-center-service",
+    label: "NYC Employment and Buisness - Centers and Services",
+    description:
+      "Find a NYC Department of Small Business Services NYC Business Solutions Center, Workforce1 Career Center, or Employment Works Center.",
+    referenceUrl:
+      "https://data.cityofnewyork.us/dataset/Center-Service-Locations/6smc-7mk6/about_data",
+    initialViewState: {
+      longitude:  -73.9710129,
+      latitude: 40.7637567,
+      zoom: 10,
+    },
+    dataUrl:
+      "https://data.cityofnewyork.us/resource/6smc-7mk6.json",
   },
 ];
-
-export function getData(dataId: string): DataOptionType | undefined {
-  return DATASET.find((d) => d.id === dataId);
-}
 
 export function getAllData(query?: string | null): DataOptionType[] {
   if (query) {
@@ -63,4 +87,8 @@ export function getAllData(query?: string | null): DataOptionType[] {
     });
   }
   return DATASET;
+}
+
+export function getData(dataId: string): DataOptionType | undefined {
+  return DATASET.find((data) => data.id === dataId);
 }
